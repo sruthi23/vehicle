00cb1143ab214715c08501898194bf344195a4f3
sruthi23 <sruthi2525@gmail.com>
2018-04-27 16:22:25 +0530

    Adding init

---
+++ chaincode/fabcar/node/fabcar.js
@@ -0,0 +1,155 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+const shim = require('fabric-shim');
+const util = require('util');
+
+let Chaincode = class {
+
+  // The Init method is called when the Smart Contract 'fabcar' is instantiated by the blockchain network
+  // Best practice is to have any Ledger initialization in separate function -- see initLedger()
+  async Init(stub) {
+    console.info('=========== Instantiated fabcar chaincode ===========');
+    return shim.success();
+  }
+
+  // The Invoke method is called as a result of an application request to run the Smart Contract
+  // 'fabcar'. The calling application program has also specified the particular smart contract
+  // function to be called, with arguments
+  async Invoke(stub) {
+    let ret = stub.getFunctionAndParameters();
+    console.info(ret);
+
+    let method = this[ret.fcn];
+    if (!method) {
+      console.error('no function of name:' + ret.fcn + ' found');
+      throw new Error('Received unknown function ' + ret.fcn + ' invocation');
+    }
+    try {
+      let payload = await method(stub, ret.params);
+      return shim.success(payload);
+    } catch (err) {
+      console.log(err);
+      return shim.error(err);
+    }
+  }
+
+  async queryCar(stub, args) {
+    if (args.length != 1) {
+      throw new Error('Incorrect number of arguments. Expecting CarNumber ex: CAR01');
+    }
+    let carNumber = args[0];
+
+    let carAsBytes = await stub.getState(carNumber); //get the car from chaincode state
+    if (!carAsBytes || carAsBytes.toString().length <= 0) {
+      throw new Error(carNumber + ' does not exist: ');
+    }
+    console.log(carAsBytes.toString());
+    return carAsBytes;
+  }
+
+  async initLedger(stub, args) {
+    console.info('============= START : Initialize Ledger ===========');
+    let cars = [];
+    cars.push({
+      make: 'Maruti',
+      model: 'Dzire',
+      year:'2008',
+      VIN:'101',
+      plate:'KL01',
+      Engine:'1.2-litre K Series VVT',
+      //color: 'blue',
+      //owner: 'Tomoko'
+    });
+    cars.push({
+      make: 'Ford',
+      model: 'Mustang',
+      year: '2010',
+      VIN: '105',
+      plate:'KL09',
+      Engine: '1.8-litre K Series VVT'
+     /* color: 'red',
+     owner: 'Brad'*/
+   });
+
+    for (let i = 0; i < cars.length; i++) {
+      cars[i].docType = 'car';
+      await stub.putState('CAR' + i, Buffer.from(JSON.stringify(cars[i])));
+      console.info('Added <--> ', cars[i]);
+    }
+    console.info('============= END : Initialize Ledger ===========');
+  }
+
+  async createCar(stub, args) {
+    console.info('============= START : Create Car ===========');
+    if (args.length != 7) {
+      throw new Error('Incorrect number of arguments.');
+    }
+
+    var car = {
+      docType: 'car',
+      make: args[1],
+      model: args[2],
+      year: args[3],
+      VIN: args[4],
+      plate: args[5],
+      engine: args[6]
+    };
+    console.info('----createCar----'+args[0]);
+    await stub.putState(args[0], Buffer.from(JSON.stringify(car)));
+    console.info('============= END : Create Car ===========');
+  }
+
+  async queryAllCars(stub, args) {
+
+    let startKey = 'CAR0';
+    let endKey = 'CAR999';
+
+    let iterator = await stub.getStateByRange(startKey, endKey);
+
+    let allResults = [];
+    while (true) {
+      let res = await iterator.next();
+
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+        console.log(res.value.value.toString('utf8'));
+
+        jsonRes.Key = res.value.key;
+        try {
+          jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+        } catch (err) {
+          console.log(err);
+          jsonRes.Record = res.value.value.toString('utf8');
+        }
+        allResults.push(jsonRes);
+      }
+      if (res.done) {
+        console.log('end of data');
+        await iterator.close();
+        console.info(allResults);
+        return Buffer.from(JSON.stringify(allResults));
+      }
+    }
+  }
+
+  async changeCarOwner(stub, args) {
+    console.info('============= START : changeCarOwner ===========');
+    if (args.length != 2) {
+      throw new Error('Incorrect number of arguments. Expecting 2');
+    }
+
+    let carAsBytes = await stub.getState(args[0]);
+    let car = JSON.parse(carAsBytes);
+    car.owner = args[1];
+
+    await stub.putState(args[0], Buffer.from(JSON.stringify(car)));
+    console.info('============= END : changeCarOwner ===========');
+  }
+};
+
+shim.start(new Chaincode());
