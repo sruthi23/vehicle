ab7f630d89ec7b0728c671694bcf52c28ffe4c01
sruthi23 <sruthi2525@gmail.com>
2018-04-30 17:55:18 +0530

    Dynamic querying

--- fabcar/query.js
+++ fabcar/query.js
@@ -27,57 +27,7 @@ var store_path = path.join(__dirname, 'hfc-key-store');
 console.log('Store path:'+store_path);
 var tx_id = null;
 
-//added
-Fabric_Client.newDefaultKeyValueStore({ path: store_path
-}).then((state_store) => {
-	// assign the store to the fabric client
-	fabric_client.setStateStore(state_store);
-	var crypto_suite = Fabric_Client.newCryptoSuite();
-	// use the same location for the state store (where the users' certificate are kept)
-	// and the crypto store (where the users' keys are kept)
-	var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
-	crypto_suite.setCryptoKeyStore(crypto_store);
-	fabric_client.setCryptoSuite(crypto_suite);
-
-	// get the enrolled user from persistence, this user will sign all requests
-	return fabric_client.getUserContext('user2', true);
-}).then((user_from_store) => {
-	if (user_from_store && user_from_store.isEnrolled()) {
-		console.log('Successfully loaded user1 from persistence');
-		member_user = user_from_store;
-	} else {
-		throw new Error('Failed to get user2.... run registerUser.js');
-	}
-
-	// queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
-	// queryAllCars chaincode function - requires no arguments , ex: args: [''],
-	const request = {
-		//targets : --- letting this default to the peers assigned to the channel
-		chaincodeId: 'fabcar',
-		fcn: 'createCar',
-		args: ['CAR2','Suzuki','Baleno','2015','108','KL04','1.0 L Boosterjet I3 turbo petrol']
-	};
-
-	// send the query proposal to the peer
-	return channel.queryByChaincode(request);
-}).then((query_responses) => {
-	console.log("Query has completed, checking results");
-	// query_responses could have more than one  results if there multiple peers were used as targets
-	if (query_responses && query_responses.length == 1) {
-		if (query_responses[0] instanceof Error) {
-			console.error("error from query = ", query_responses[0]);
-		} else {
-			console.log("Response is ", query_responses[0].toString());
-		}
-	} else {
-		console.log("No payloads were returned from query");
-	}
-}).catch((err) => {
-	console.error('Failed to query successfully :: ' + err);
-});
-
-//end
-
+function allCarDetails(arg){
 // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
 Fabric_Client.newDefaultKeyValueStore({ path: store_path
 }).then((state_store) => {
@@ -91,23 +41,27 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 	fabric_client.setCryptoSuite(crypto_suite);
 
 	// get the enrolled user from persistence, this user will sign all requests
-	return fabric_client.getUserContext('user1', true);
+	console.log(arg.user);
+	return fabric_client.getUserContext(arg.user, true);
 }).then((user_from_store) => {
 	if (user_from_store && user_from_store.isEnrolled()) {
-		console.log('Successfully loaded user1 from persistence');
+		console.log('Successfully loaded' +arg.user+' from persistence');
 		member_user = user_from_store;
 	} else {
-		throw new Error('Failed to get user1.... run registerUser.js');
+		throw new Error('Failed to get '+ arg.user + ' .... run registerUser.js');
 	}
 
 	// queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
 	// queryAllCars chaincode function - requires no arguments , ex: args: [''],
+
+	
 	const request = {
 		//targets : --- letting this default to the peers assigned to the channel
 		chaincodeId: 'fabcar',
-		fcn: 'queryAllCars',
-		args: ['']
-	};
+		fcn: arg.func,
+		args: arg.data
+	}; 
+	console.log(request);
 
 	// send the query proposal to the peer
 	return channel.queryByChaincode(request);
@@ -118,7 +72,10 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 		if (query_responses[0] instanceof Error) {
 			console.error("error from query = ", query_responses[0]);
 		} else {
+			
 			console.log("Response is ", query_responses[0].toString());
+			//return(query_responses[0]);
+
 		}
 	} else {
 		console.log("No payloads were returned from query");
@@ -126,3 +83,5 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 }).catch((err) => {
 	console.error('Failed to query successfully :: ' + err);
 });
+}
+module.exports.allCarDetails = allCarDetails;
