63314ac807520ef8fa9a283e6f3708c7f6575385
sruthi23 <sruthi2525@gmail.com>
2018-05-30 10:23:20 +0530

    Port changed

--- fabcar/server.js
+++ fabcar/server.js
@@ -22,7 +22,7 @@ console.log(replaceinfo)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8081;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
