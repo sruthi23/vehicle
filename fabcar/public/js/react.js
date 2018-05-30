b6f375fb3a5e8c75cad4a2fe5d79ba2884e17c23
sibizulu <sibiraj@lightrains.com>
2018-05-30 16:56:46 +0530

    Adding ui

--- fabcar/public/js/react.js
+++ fabcar/public/js/react.js
@@ -14,88 +14,133 @@ class Greeting extends React.Component {
       'data[]': 'X4SD23FERTS34DF'
     }
 
-    var formBody = [];
+    var formBody = []
     for (var property in args) {
-      var encodedKey = encodeURIComponent(property);
-      var encodedValue = encodeURIComponent(args[property]);
-      formBody.push(encodedKey + "=" + encodedValue);
+      var encodedKey = encodeURIComponent(property)
+      var encodedValue = encodeURIComponent(args[property])
+      formBody.push(encodedKey + '=' + encodedValue)
     }
-    formBody = formBody.join("&");
+    formBody = formBody.join('&')
 
-    fetch('http://localhost:8081/api/query', {
-     method: 'post',
-     headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-     body: formBody
-   })
+    fetch('http://ec2-54-191-119-86.us-west-2.compute.amazonaws.com:8081/api/query', {
+      method: 'post',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
+      body: formBody
+    })
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(res_data => this.setState({data: res_data.res}))
   }
 
   render () {
-    return (
-      <div>
-      <h1>Nissan Terrano</h1>
-      <div className='ui grid'>
-      <div className='ten wide column'>
-      <p>Nissan Terrano price starts at ₹ 9.99 Lakhs and goes upto ₹ 14.48 Lakhs. Petrol Terrano price starts at ₹ 9.99 Lakhs. Diesel Terrano price starts at ₹ 9.99 Lakhs.</p>
-      <b>VIN: 1HGCM567X5A034579</b>
-      </div>
-      <div className='six wide column'>
-      <img src='https://imgd.aeplcdn.com/1056x594/cw/ec/28376/Nissan-Terrano-Front-view-93548.jpg?v=201711021421&q=80' alt='' width='100%' />
-      </div>
-      </div>
+    console.log(this.state.data)
+    const statusName = ['On time', '10% Deviation', '11% to 25% Deviation']
+    const {data} = this.state
 
-      <div className='ui hidden divider' />
-      <div className='ui divider' />
-      <div className='ui hidden divider' />
+    // const leng = (data.services) ? Object.keys(data.services[0]).length : 0
+    // console.log(leng)
+    // (data.services)console.log(data.services[0])
 
-      <div className='ui cards' id='blocks'>
-      <div className='ui top attached tabular menu'>
-      <a className='item active' data-tab='first'>Service Reports</a>
-      <a className='item' data-tab='second'>Part Replacements</a>
-      </div>
-      <div className='ui bottom attached tab segment active' data-tab='first'>
-      <table className='ui fixed single line celled table'>
-      <thead>
-      <tr>
-      <th>Service Schedule</th>
-      <th>Scheduled </th>
-      <th>Actual </th>
-      <th>On time 10% </th>
-      <th>Deviation 11% to 25% Deviation</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-      <td>John</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      </tr>
-      <tr>
-      <td>John</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      </tr>
-      <tr>
-      <td>John</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      <td>Approved</td>
-      </tr>
-      </tbody>
-      </table>
-      </div>
-      <div className='ui bottom attached tab segment' data-tab='second'>
-      Second
-      </div>
-      </div>
-      </div>
+    if (data.services) {
+      var rows = Object.keys(data.services[0]).map(function (key) {
+        var row = data.services[0][key]
+        return (<tr key={key}>
+          <td>{key.replace('_', ' ').toUpperCase()}</td>
+          <td>{row.schedule}</td>
+          <td>{row.actual}</td>
+          <td>{row.points}</td>
+          <td>{statusName[row.status]}</td>
+        </tr>)
+      })
+    }
+    var rows_replacement = ''
+    if (data.replacement) {
+      var rows_replacement = data.replacement.map(function (v, k) {
+        var keyValue = 'part_' + (k + 1)
+
+        // console.log(data.replacement[k][keyValue])
+
+        return data.replacement[k][keyValue].map(function (vd, kd) {
+          var time = new Date(+vd.date)
+          console.log(vd, kd, time, +vd.date)
+          return (<tr key={keyValue + kd}>
+            {kd === 0 && <td rowSpan={data.replacement[k][keyValue].length}>PART {(k + 1)}</td>}
+            <td>{time.toString()}</td>
+            <td>{vd.schedule}</td>
+            <td>{vd.actual}</td>
+            <td>{vd.points}</td>
+            <td>{statusName[vd.status]}</td>
+          </tr>)
+        })
+      })
+    }
+
+    {
+      return (
+        <div>
+          <h1>{data.make} {data.model}</h1>
+          <div className='ui grid'>
+            <div className='ten wide column'>
+              <p>The all new Nissan Terrano comes with 22 new features which make you drool for it even more. Made to dominate every terrain, this all-rounder of an SUV is power-packed with a diesel engine, producing enough power to tackle every obstacle on road.</p>
+
+              <b>VIN: {data.VIN}</b>
+            </div>
+            <div className='six wide column'>
+              <img src='https://imgd.aeplcdn.com/1056x594/cw/ec/28376/Nissan-Terrano-Front-view-93548.jpg?v=201711021421&q=80' alt='' width='100%' />
+            </div>
+          </div>
+
+          <div className='ui hidden divider' />
+          <div className='ui divider' />
+          <div className='ui hidden divider' />
+
+          <div className='ui cards' id='blocks'>
+            <div className='ui top attached tabular menu'>
+              <a className='item active' data-tab='first'>Service Reports</a>
+              <a className='item' data-tab='second'>Part Replacements</a>
+            </div>
+            <div className='ui bottom attached tab segment active' data-tab='first'>
+              <table className='ui fixed single line celled table'>
+                <thead>
+                  <tr>
+                    <th>Service Schedule</th>
+                    <th>Scheduled </th>
+                    <th>Actual </th>
+                    <th>Points</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  {rows}
+
+                </tbody>
+              </table>
+
+            </div>
+            <div className='ui bottom attached tab segment' data-tab='second'>
+
+              <table className='ui fixed single line celled table'>
+                <thead>
+                  <tr>
+                    <th>Part Replacement</th>
+                    <th>Date</th>
+                    <th>Scheduled Replacement</th>
+                    <th>Actual</th>
+                    <th>Points</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  {rows_replacement}
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       )
+    }
   }
 }
 ReactDOM.render(
--- fabcar/public/js/script.js
+++ fabcar/public/js/script.js
@@ -1,20 +1,158 @@
 'use strict'
 
-var urlBase = 'http://ec2-54-191-119-86.us-west-2.compute.amazonaws.com:8081'
-var addUrl = urlBase + '/api/invoke'
-var searchUrl = urlBase + '/api/query'
-
 $(function () {
-  // /loadData('X4SD23FERTS34DF')
+  var urlBase = 'http://localhost:8080'
+  var addUrl = urlBase + '/api/invoke'
+  var searchUrl = urlBase + '/api/query'
+  var ownerChangeUrl = urlBase + '/api/changeowner'
+  var UserRegUrl = urlBase + '/api/registerUser'
+
+  $('#add_vin').val(makeid())
+
+  // Add Vehicle Form
+  $('#addVehicle').submit(function (e) {
+    e.preventDefault()
+    var values = $(this).serialize()
+    $(this).addClass('loading')
+    $.ajax({
+      type: 'POST',
+      url: addUrl,
+      data: values,
+      success: function (data) {
+        window.location.href = 'addvehicle.html'
+      }
+    })
+  })
+
+  // Change Owner
+  $('#changeOwnerFrm').submit(function (e) {
+    e.preventDefault()
+    var values = $(this).serialize()
+    var vin = $('#vin_num').val()
+    $.ajax({
+      type: 'POST',
+      url: UserRegUrl,
+      data: values,
+      success: function (result) {
+        var data = [vin, result.res]
+        console.log(data)
+
+        $.ajax({
+          type: 'POST',
+          url: ownerChangeUrl,
+          data: { data: data },
+          success: function (succ) {
+            alert(succ.res)
+            $(this).trigger('reset')
+          }
+        })
+      }
+    })
+  })
+
+  $('#actntemplate a.ui.card').click(function (e) {
+    e.preventDefault()
+    $(this)
+      .appendTo('#history')
+      .hide()
+      .show('slow')
+    // $.ajax({
+    //   type: 'POST',
+    //   url: addUrl,
+    //   data: values,
+    //   success: function(data) {
+    //     window.location.href = 'addvehicle.html'
+    //   }
+    // })
+  })
+
+  $('.menu .item').tab()
+
+  // Search
+  $('#searchForm').submit(function (e) {
+    e.preventDefault()
+    var data = []
+    data.push($('#search_vin').val())
+    $.ajax({
+      type: 'POST',
+      url: searchUrl,
+      data: { func: 'queryCar', user: 'John', data: data },
+      success: function (data) {
+        var item = data.res
+
+        var html =
+          '<div class="ui fluid green segment"> \
+        <table class="ui very basic celled table"> \
+        <tr> \
+        <td>VIN</td> \
+        <td>' +
+          item.VIN +
+          '</td> \
+        </tr> \
+        <tr> \
+        <td>Make</td> \
+        <td>' +
+          item.make +
+          '</td> \
+        </tr> \
+        <tr> \
+        <td>Model</td> \
+        <td>' +
+          item.model +
+          '</td> \
+        </tr> \
+        <tr> \
+        <td>Year</td> \
+        <td>' +
+          item.year +
+          '</td> \
+        </tr> \
+        <tr> \
+        <td>Plate</td> \
+        <td>' +
+          item.plate +
+          '</td> \
+        </tr> \
+        <tr> \
+        <td>Engine</td> \
+        <td>' +
+          item.engine +
+          '</td> \
+        </tr> \
+        </table> \
+        </div>\
+        <div class="ui fluid red segment">\
+        <table class="ui very basic celled table">\
+        <tr>\
+        <td>Owner</td>\
+        <td>' +
+          item.owner +
+          '</td>\
+        </tr>\
+        </table>\
+        </div>'
+
+        $('.live_result').html(html)
+        $('#vin_num').val(item.VIN)
+
+        $('.results').css('display', 'block')
+        $(this).trigger('reset')
+      }
+    })
+  })
+
+  $('#changeOwnerBtn').click(function () {
+    $('#changeOwnerFrm').css('display', 'block')
+  })
 })
 
-function loadData (vin) {
-  $.ajax({
-    type: 'POST',
-    url: searchUrl,
-    data: {func: 'queryCar', 'data[]': vin},
-    success: function (data) {
-      console.log(data.res)
-    }
-  })
+function makeid () {
+  var text = ''
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+  for (var i = 0; i < 16; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+
+  return text
 }
